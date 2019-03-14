bd0dc69500807d7ea5affff1d53e2655a870d6a2
HarlamovBuldog <vladwoode@gmail.com>
2019-03-14 10:39:24 +0300

    Added some useful comments as well as task 1.4 implementation from book

--- Book_exercises/Introdution/reading_info_from_files/dup3_add_task.go
+++ Book_exercises/Introdution/reading_info_from_files/dup3_add_task.go
@@ -1,7 +1,10 @@
-//Dup2 prints text of each line, which
-//appears in input more then 1 times.
 //Programm reads standart input or list of named files.
-
+//If this is standart input programm simply prints lines
+//which appears more then 1 time.
+//If these are files then program counts how many times
+//each string appears in program for each file and also
+//says if there are any repeated strings.
+//Note: Task 1.4 realization from book page 34
 package main
 
 import (
@@ -17,7 +20,9 @@ func main() {
 		countLines(os.Stdin, counts)
 	} else {
 		for _, arg := range files {
+			doLinesRepeat := false
 			countsForEachFile := make(map[string]int)
+
 			f, err := os.Open(arg)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, arg+": %v\n", err)
@@ -26,26 +31,33 @@ func main() {
 			countLines(f, counts)
 			f.Close()
 
-			f1, err1 := os.Open(arg)
+			f1, err := os.Open(arg)
 			fmt.Println(arg + ":")
-			if err1 != nil {
-				fmt.Fprintf(os.Stderr, arg+": %v\n", err1)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, arg+": %v\n", err)
 				continue
 			}
 			countLines(f1, countsForEachFile)
 			f1.Close()
 
 			for line, n := range countsForEachFile {
-				if len(countsForEachFile) > 1 {
-					fmt.Printf("%d\t%s\n", n, line)
+				fmt.Printf("%d\t%s\n", n, line)
+				if doLinesRepeat != true && n > 1 {
+					doLinesRepeat = true
 				}
+			}
+
+			if doLinesRepeat {
+				fmt.Println("File " + arg + " has repeated lines")
+			} else {
+				fmt.Println("File " + arg + " doesn't have repeated lines")
 			}
 		}
 	}
 
 	fmt.Println("Total:")
 	for line, n := range counts {
-		if len(counts) > 1 {
+		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
