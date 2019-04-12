6b69f74f9edf75e3de2969c38f0b647d03d69ffd
HarlamovBuldog <vladwoode@gmail.com>
2019-04-12 10:09:37 +0300

    Custom functions start- and endElement were created. They server as arguments of function ForEachNode from "link" package

--- src/Book_exercises/chapter5/outline.go
+++ src/Book_exercises/chapter5/outline.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"Book_exercises/chapter5/links"
 	"fmt"
 	"os"
 
 	"golang.org/x/net/html"
 )
+
+var depth int
 
 func main() {
 	doc, err := html.Parse(os.Stdin)
@@ -14,6 +17,8 @@ func main() {
 		os.Exit(1)
 	}
 	outline(nil, doc)
+
+	links.ForEachNode(doc, startElement, endElement)
 }
 
 func outline(stack []string, n *html.Node) {
@@ -24,5 +29,19 @@ func outline(stack []string, n *html.Node) {
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		outline(stack, c)
+	}
+}
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
+	}
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		depth--
+		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
 	}
 }
