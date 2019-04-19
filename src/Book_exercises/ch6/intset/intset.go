dc0db607bd7ff817016e17a604ec7df828cd9926
HarlamovBuldog <vladwoode@gmail.com>
2019-04-19 17:16:48 +0300

    Bit vector represantation

---
+++ src/Book_exercises/ch6/intset/intset.go
@@ -0,0 +1,59 @@
+package intset
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// IntSet is set of small non-negative.
+// integer numbers. Null value is empty set.
+type IntSet struct {
+	words []uint64
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// Has returns true if the set contains a
+// non-negative value of x
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds non-negative value x to set
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith makes set s to be union of set s and set t
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
