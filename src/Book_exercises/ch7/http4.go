d6ad4323bda891305a6173ed949a69aaa1f768c4
HarlamovBuldog <vladwoode@gmail.com>
2019-05-16 15:40:23 +0300

    Realization of exercises 7.11 and 7.12 from book was finished. Some useful comments about what is going on were added

--- src/Book_exercises/ch7/http4.go
+++ src/Book_exercises/ch7/http4.go
@@ -1,11 +1,35 @@
+// Exercise 7.11 and 7.12 realization from book page 236
+// Create, update, delete functions were added to work with map
+// "List" function was improved to show data in table using html/template
+// Mutex was added to prevent simultaneous data changes
 package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 )
+
+var mu sync.Mutex
+
+var goodsHTMLTable = template.Must(template.New("goodsHTMLTable").Parse(`
+<h1>{{ len . }} items</h1>
+<table border="1">
+<tr style='text-align: left'>
+	<th>Item</th>
+	<th>Price</th>
+</tr>
+{{ range $item, $price := . }}
+<tr>
+	<td>{{ $item }}</td>
+	<td>{{ $price }}</td>
+</tr>
+{{ end }}
+</table>
+`))
 
 func main() {
 	db := database{"shoes": 50, "socks": 5}
@@ -18,6 +42,7 @@ func main() {
 	mux.Handle("/price", http.HandlerFunc(db.price))
 	mux.Handle("/create", http.HandlerFunc(db.create))
 	mux.Handle("/update", http.HandlerFunc(db.update))
+	mux.Handle("/delete", http.HandlerFunc(db.delete))
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
 
@@ -27,15 +52,28 @@ func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
 
 type database map[string]dollars
 
+func (db database) totalCount() int {
+	return len(db)
+}
+
 func (db database) list(w http.ResponseWriter, req *http.Request) {
-	for item, price := range db {
-		fmt.Fprintf(w, "%s: %s\n", item, price)
+	mu.Lock()
+	if err := goodsHTMLTable.Execute(w, &db); err != nil {
+		log.Fatal(err)
 	}
+	/*
+		for item, price := range db {
+			fmt.Fprintf(w, "%s: %s\n", item, price)
+		}
+	*/
+	mu.Unlock()
 }
 
 func (db database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
+	mu.Lock()
 	price, ok := db[item]
+	mu.Unlock()
 	if !ok {
 		w.WriteHeader(http.StatusNotFound) // 404
 		fmt.Fprintf(w, "no such item: %q\n", item)
@@ -46,7 +84,9 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 
 func (db database) create(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
+	mu.Lock()
 	_, ok := db[item]
+	mu.Unlock()
 	if !ok {
 		priceStr := req.URL.Query().Get("price")
 		priceFloat, err := strconv.ParseFloat(priceStr, 32)
@@ -54,8 +94,10 @@ func (db database) create(w http.ResponseWriter, req *http.Request) {
 			fmt.Fprintf(w, "wrong price: %s\n", priceStr)
 			return
 		}
+		mu.Lock()
 		db[item] = dollars(float32(priceFloat))
 		fmt.Fprintf(w, "%s item with price %s was successfully added!\n", item, db[item])
+		mu.Unlock()
 		return
 	}
 	fmt.Fprintf(w, "item with name \"%s\" already exist!\nIf u wanted to update item use /update\n", item)
@@ -63,7 +105,9 @@ func (db database) create(w http.ResponseWriter, req *http.Request) {
 
 func (db database) update(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
+	mu.Lock()
 	oldPrice, ok := db[item]
+	mu.Unlock()
 	if ok {
 		priceStr := req.URL.Query().Get("price")
 		priceFloat, err := strconv.ParseFloat(priceStr, 32)
@@ -71,9 +115,26 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 			fmt.Fprintf(w, "wrong price: %s\n", priceStr)
 			return
 		}
+		mu.Lock()
 		db[item] = dollars(float32(priceFloat))
 		fmt.Fprintf(w, "%s item's price %s was successfully updated to %s!\n", item, oldPrice, db[item])
+		mu.Unlock()
 		return
 	}
 	fmt.Fprintf(w, "item with name \"%s\" doesn't exist!\n", item)
+}
+
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	mu.Lock()
+	price, ok := db[item]
+	mu.Unlock()
+	if ok {
+		mu.Lock()
+		delete(db, item)
+		mu.Unlock()
+		fmt.Fprintf(w, "Item %s with price %s was successfully deleted!\n", item, price)
+		return
+	}
+	fmt.Fprintf(w, "Item with name \"%s\" doesn't exist!\n", item)
 }
