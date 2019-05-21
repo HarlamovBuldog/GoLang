f983edc049b79e374bbda76adc794e28ef9d0227
HarlamovBuldog <vladwoode@gmail.com>
2019-05-21 16:40:12 +0300

    "Check" methods for all types which implement "Expr" interface were added

---
+++ src/Book_exercises/ch7/eval/check.go
@@ -0,0 +1,51 @@
+package myeval
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
+func (literal) Check(vars map[Var]bool) error {
+	return nil
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("invalid unary operator %q", u.op)
+	}
+	return u.x.Check(vars)
+}
+
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.op) {
+		return fmt.Errorf("invalid binary operator %q", b.op)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.fn]
+	if !ok {
+		return fmt.Errorf("Unknown function %q", c.fn)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call of %s has %d instead of %d arguments",
+			c.fn, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
