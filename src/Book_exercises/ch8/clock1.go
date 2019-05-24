dc1c3ffcfd887d6f690fb0933cac030d35b3904f
HarlamovBuldog <vladwoode@gmail.com>
2019-05-24 10:58:54 +0300

    Example of parallel processing in interactions of simple tcp-server with tcp-client

---
+++ src/Book_exercises/ch8/clock1.go
@@ -0,0 +1,37 @@
+// Clock1 presents TCP-server,
+// which periodically shows time.
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // for example, connection lost
+			continue
+		}
+		//handleConn(conn)    // the only one connection processing
+		go handleConn(conn) // for parallel processing
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // for example, client disconnection
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
