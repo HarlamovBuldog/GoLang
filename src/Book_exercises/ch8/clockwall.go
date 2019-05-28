571d8a3c9bd15954310a75de612425ac8e6c89c2
HarlamovBuldog <vladwoode@gmail.com>
2019-05-28 17:19:57 +0300

    Updated tcp-client clockwall in order to receive data from multiple tcp-servers

--- src/Book_exercises/ch8/clockwall.go
+++ src/Book_exercises/ch8/clockwall.go
@@ -1,55 +1,75 @@
-// Netcat1 - TCP-client for readonly.
+// Clockwall.go is TCP-client for handling data
+// from connections to multiple servers.
+// Exercise 8.1 realization from book page 265
 package main
 
 import (
 	"fmt"
-	"html/template"
 	"io"
 	"log"
 	"net"
 	"os"
+	"os/exec"
 	"regexp"
 	"strings"
+	"time"
 )
 
-var clockWall = template.Must(template.New("clockWall").Parse(`
-<h1>{{.TotalCount}} themes</h1>
-<table>
-<tr style='text-align: left'>
-	<th>#</th>
-	<th>State</th>
-	<th>User</th>
-	<th>Title</th>
-</tr>
-{{range .Items}}
-<tr>
-	<td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
-	<td>{{.State}}</td>
-	<td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
-	<td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
-</tr>
-{{end}}
-</table>
-`))
+type TimeTable struct {
+	curTime []byte
+}
+
+func (tt *TimeTable) Write(data []byte) (n int, err error) {
+	tt.curTime = data
+	return len(data), nil
+}
+
+func (tt TimeTable) String() string {
+	return string(tt.curTime)
+}
 
 func main() {
+	var areaNames []string
+	var localhosts []string
 	var validArg = regexp.MustCompile(`[a-zA-z]+=localhost:(6553[0-5]|655[0-2][0-9]\d|65[0-4](\d){2}|6[0-4](\d){3}|[1-5](\d){4}|[1-9](\d){0,3})`)
 	for _, argToParse := range os.Args[1:] {
 		if validArg.MatchString(argToParse) == true {
 			// go func here
 			indexOfEqualSign := strings.Index(argToParse, "=")
-			areaName := argToParse[:indexOfEqualSign]
-			localhostStr := argToParse[indexOfEqualSign+1:]
-			fmt.Printf("Area: %s\nHost: %s\n", areaName, localhostStr)
-			conn, err := net.Dial("tcp", localhostStr)
+
+			iterAreaName := argToParse[:indexOfEqualSign]
+			areaNames = append(areaNames, iterAreaName)
+
+			iterLocalhost := argToParse[indexOfEqualSign+1:]
+			localhosts = append(localhosts, iterLocalhost)
+
+			//fmt.Println("Area: %s\nHost: %s\n", iterAreaName, iterLocalhost)
+		} else {
+			fmt.Printf("%s: invalid call\n", argToParse)
+		}
+	}
+
+	for {
+		for _, areaName := range areaNames {
+			fmt.Print(areaName + "\t")
+		}
+		fmt.Println("\n----------------------------------------------")
+
+		for _, localhost := range localhosts {
+			conn, err := net.Dial("tcp", localhost)
 			if err != nil {
 				log.Fatal(err)
 			}
 			defer conn.Close()
-			mustCopy(os.Stdout, conn)
-		} else {
-			fmt.Printf("%s: invalid call\n", argToParse)
+			timeTable := TimeTable{}
+			mustCopy(&timeTable, conn)
+			fmt.Printf("%s\t", timeTable)
 		}
+		fmt.Println("")
+		time.Sleep(1 * time.Second)
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
 	}
 }
 
