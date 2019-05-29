9ef79e1776d30dfa912f4887a2aedce3e3e7ad44
HarlamovBuldog <vladwoode@gmail.com>
2019-05-29 17:42:27 +0300

    Unidirectional channels usage example from book

---
+++ src/Book_exercises/ch8/pipeline3.go
@@ -0,0 +1,37 @@
+// Unidirectional channels usage example from book
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func counter(out chan<- int) {
+	for x := 0; x < 15; x++ {
+		out <- x
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for x := range in {
+		out <- x * x
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for x := range in {
+		fmt.Println(x)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go counter(naturals)
+	go squarer(squares, naturals)
+	printer(squares)
+}
